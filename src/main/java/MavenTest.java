43208118430b94ed0f10fd927ea6fe3ecd1bf88f
sunny-kong <821879144@qq.com>
2015-12-06 19:39:40 +0800

    项目初成

---
+++ src/main/java/MavenTest.java
@@ -0,0 +1,14 @@
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * Created by xj on 15-12-6.
+ */
+public class MavenTest {
+    public static void main(String[] args) {
+        System.out.println(NumberUtils.toInt("13"));
+        System.out.println(NumberUtils.toInt("22"));
+
+
+    }
+
+}
